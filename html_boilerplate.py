f68d9217e7443452ee45d0c78f072999b42d095c
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-08 21:53:48 -0500

    Bug fixes. Add minor improvement
    Fix bugs where new .css and .js files create in the wrong directory with wrong names
    add variable path outside of function
    add condition to check whether a path is provided or not

--- html_boilerplate.py
+++ html_boilerplate.py
@@ -5,19 +5,22 @@ __location__ = os.path.realpath(
 html_path = os.path.join(__location__, 'html.txt')
 
 html = ''
+path = ''
+
 with open(html_path) as f:
     html = f.read()
 
-def generate_files():
-    path = input(r'Please enter the path where you want to create your new files (hint:use shift + ins to paste if you are using a terminal such as git bash): ')
+def generate_files(path):
+    if not path:
+        path = input(r'Please enter the path where you want to create your new files (hint:use shift + ins to paste if you are using a terminal such as git bash): ')
     project_name = input(r'Enter the project name: ')
     if '\\' in path:
         path = path.replace('\\', '/')
 
     new_path = os.path.join(path, project_name + '/')
-    path_resource = os.path.join(new_path, 'resource')
-    path_CSS = os.path.join(path_resource, 'CSS')
-    path_javaScript = os.path.join(path_resource, 'Script');
+    path_resource = os.path.join(new_path, 'resource/')
+    path_CSS = os.path.join(path_resource, 'CSS/')
+    path_javaScript = os.path.join(path_resource, 'Script/');
 
     os.mkdir(new_path)
     os.mkdir(path_resource)
@@ -34,4 +37,4 @@ def generate_files():
     exit_file = (input(r'File generation complete. Press enter to exit. '))
 
 
-generate_files()
+generate_files(path)
