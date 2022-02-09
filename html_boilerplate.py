4b4ff5986c53c72e68612d68a249e994c0efea8e
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-08 21:31:00 -0500

    Add function for file generation
    Add generate_file() with the following functionality:
    + create a directory to hold the project
    + create directory "/resource" for CSS and JavaScript files
    + create empty .css and .js files
    + create index.html with boilerplate code

--- html_boilerplate.py
+++ html_boilerplate.py
@@ -1,10 +1,33 @@
 import os
 
-path = ''
-def replace_all():
-    x = input(r'enter the string you want to replace ')
-    y = input(r'enter the symbol you want to replace ')
-    z= input(r'enter the symbol you want {0} to be replaced as '.format(y))
-    x = x.replace(y, z)
-    print ("here's the text:", x)
-    return x
+html = ''
+with open('./html.txt') as f:
+    html = f.read()
+
+def generate_files():
+    path = input(r'Please enter the path where you want to create your new files ')
+    project_name = input(r'Enter the project name ')
+    if '\\' in path:
+        path = path.replace('\\', '/')
+
+    new_path = os.path.join(path, project_name + '/')
+    path_resource = os.path.join(new_path, 'resource')
+    path_CSS = os.path.join(new_path_resource, 'CSS')
+    path_javaScript = os.path.join(new_path_resource, 'Script');
+
+    os.mkdir(new_path)
+    os.mkdir(new_path_resource)
+    os.mkdir(path_CSS)
+    os.mkdir(path_javaScript)
+
+    with open(new_path + "index.html", 'x') as f:
+        f.write(html)
+    with open(path_javaScript + "main.js", 'x') as f:
+        f.write(' ')
+    with open(path_CSS + "style.css", 'x') as f:
+        f.write(' ')
+    
+    exit_file = (input(r'File generation complete. Press enter to exit. '))
+
+
+generate_files()
